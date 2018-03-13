44abeb17d55e903d2086239f401ddb31bd9c81ca
Victoria Suleymanova <viki-82@mail.ru>
2018-03-13 21:25:19 +0700

    logging was added to instagram service

--- bot-service-akka/src/main/scala/com/bartender/bot/service/instagram/InstagramService.scala
+++ bot-service-akka/src/main/scala/com/bartender/bot/service/instagram/InstagramService.scala
@@ -4,7 +4,7 @@ import java.io.File
 import java.net.URL
 
 import akka.actor.ActorSystem
-import com.bartender.bot.service.common.{Config, Dependency}
+import com.bartender.bot.service.common.{Config, Dependency, Logging}
 import com.bartender.bot.service.services.CocktailResearcher
 import org.apache.commons.io.FileUtils
 import org.brunocvcunha.instagram4j.Instagram4j
@@ -15,14 +15,16 @@ import scala.concurrent.duration._
 
 
 class InstagramClient(instagramApi: Instagram4j,
-                      cocktailResearcher: CocktailResearcher) {
+                      cocktailResearcher: CocktailResearcher) extends Logging {
 
-  def publishRandomCocktail(): Boolean = {
-    cocktailResearcher.cocktailReceipt(None).exists(cocktail =>
-      cocktail._1.imageUrl.exists(url => {
-        val destination = new File("/tmp/cocktail_of_day.jpg")
-        FileUtils.copyURLToFile(new URL(url), destination)
-        instagramApi.sendRequest(new InstagramUploadPhotoRequest(destination,
+  def publishRandomCocktail(): Boolean = cocktailResearcher.cocktailReceipt(None).exists(cocktail =>
+    cocktail._1.imageUrl.exists(url => {
+      val destination = new File("/tmp/cocktail_of_day.jpg")
+      rootLogger.info(s"Cocktail picture uploded from $url")
+      FileUtils.copyURLToFile(new URL(url), destination)
+      rootLogger.info(s"Instagram send request")
+      try {
+        val result = instagramApi.sendRequest(new InstagramUploadPhotoRequest(destination,
           s"""| ${cocktail._1.name}
               | ${
             cocktail._2.ingredients.map(ingredient =>
@@ -36,26 +38,36 @@ class InstagramClient(instagramApi: Instagram4j,
           }
               | ${cocktail._2.instruction.map(_.toString).getOrElse("")}
               | #${cocktail._1.name.replaceAll(" ", "")} #cocktail #coctailofday #cocktails #tony🍹
-            """.stripMargin))
-        return true
-      }))
-  }
+          """.stripMargin))
+        rootLogger.info(s"uploading result: ${result.toString}")
+      } catch {
+        case e: Exception => rootLogger.error(e.getMessage)
+      }
+      return true
+    }))
 }
 
-object InstagramService extends Config with Dependency {
+object InstagramService extends Config with Dependency with Logging {
   private lazy val instagramClient = {
     val instagram4j = Instagram4j.builder()
       .username(instagramConfig.getString("username"))
       .password(instagramConfig.getString("password"))
       .build()
+    rootLogger.info("Instagram setup")
     instagram4j.setup()
+    rootLogger.info("Instagram login")
     instagram4j.login()
     new InstagramClient(instagram4j, cocktailResearcher)
   }
 
   def sendRandomCocktailEveryDay(): Unit = {
     val system = ActorSystem("MySystem")
-    system.scheduler.schedule(0 day, 1 day)(instagramClient.publishRandomCocktail())
+    system.scheduler.schedule(0 day, 1 day) {
+      rootLogger.info("Publish random cocktail..")
+      val result = instagramClient.publishRandomCocktail()
+      if (result) rootLogger.info("Publish random cocktail was successful")
+      else rootLogger.info("Instagram post was not created")
+    }
   }
 }
 
